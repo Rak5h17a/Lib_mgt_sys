b7cf59849ae2605ab07d2d44a111509334331c6d
Rak5h17a <rr.rakshita.rr@gmail.com>
2026-08-12 17:59:02 +0530

    feat Added JWT token creation and verification.

--- library-api/app/core/config.py
+++ library-api/app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     MONGODB_URL: str 
     DATABASE_NAME: str 
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int =60
 
     model_config=SettingsConfigDict(
         env_file=".env",
--- library-api/app/core/security.py
+++ library-api/app/core/security.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+
+from app.core.config import settings
+
+ALGORITHM = "HS256"
 
 #this hash tool uses bcrypt
 _pwd_context= CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -8,3 +14,21 @@ def hash_password(plain_password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return _pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(user_id: str, role: str) -> str:
+    #creates signed jwt containing user id and role.
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
+        "sub": user_id,
+        "role": role,
+        "exp": expire,
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str) -> dict | None:
+    #verify jwt signature and expiry
+    try: 
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
