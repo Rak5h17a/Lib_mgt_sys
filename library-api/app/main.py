0e7aba141024aa8072d3c156d5263fb6e50c41fa
Rak5h17a <rr.rakshita.rr@gmail.com>
2026-08-17 12:30:36 +0530

    feat: Added CORS middleware to allow frontend requests.

--- library-api/app/main.py
+++ library-api/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import connect_to_mongo, close_mongo_connection
 from app.api.routes import items, members, loans, auth
@@ -11,6 +12,15 @@ async def lifespan(app: FastAPI):
     await close_mongo_connection()
 
 app= FastAPI(title="Library Management API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(items.router)
 app.include_router(members.router)
 app.include_router(loans.router)
